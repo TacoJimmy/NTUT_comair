7cdeac09b7d2146ae31b4bd28627f66b1e172fc4
TacoJimmy <t5669016@ntut.org.tw>
2022-09-07 09:38:28 +0800

    Update comfair.py

--- comfair.py
+++ comfair.py
@@ -35,6 +35,58 @@ def on_message(client, userdata, msg):
     print(data_payload)
     comf_set = data_payload['params']
     #j = json.loads(data)
+    
+    RTcond = get_temp()
+    num_comfort = comfort_defin(RTcond[0],RTcond[1],3)
+    print (num_comfort)
+    print (RTcond)
+    print (set_speed(RTcond[0],RTcond[1]))
+    payload = {'Temperature' : evm_temp, 'Humidity':evm_humi}
+    client02.publish("v1/devices/me/telemetry", json.dumps(payload))
+
+    time.sleep(5)
+    
+    if evm_velocity > 3:
+        temp_set = temp_set - 1
+        if temp_set == 20:
+            AC_20()
+        elif temp_set == 21:
+            AC_21()
+        elif temp_set == 22:
+            AC_22()
+        elif temp_set == 23:
+            AC_23()
+        elif temp_set == 24:
+            AC_24()
+        elif temp_set == 25:
+            AC_25()
+        elif temp_set == 26:
+            AC_26()
+        elif temp_set == 27:
+            AC_27()
+            
+    if evm_velocity < 2:
+        temp_set = temp_set + 1
+        if temp_set == 20:
+            AC_20()
+        elif temp_set == 21:
+            AC_21()
+        elif temp_set == 22:
+            AC_22()
+        elif temp_set == 23:
+            AC_23()
+        elif temp_set == 24:
+            AC_24()
+        elif temp_set == 25:
+            AC_25()
+        elif temp_set == 26:
+            AC_26()
+        elif temp_set == 27:
+            AC_27()
+    time.sleep(5)
+    
+    
+    
     listtopic = data_topic.split("/") 
 
 
@@ -167,53 +219,5 @@ if __name__ == '__main__':
     client02.username_pw_set("IZcJiw4YcQFvDyBno9pd","xxxx")
     client02.connect("thingsboard.cloud", 1883, 60)
     
-    while True:
-        RTcond = get_temp()
-        num_comfort = comfort_defin(RTcond[0],RTcond[1],3)
-        print (num_comfort)
-        print (RTcond)
-        print (set_speed(RTcond[0],RTcond[1]))
-        payload = {'Temperature' : evm_temp, 'Humidity':evm_humi}
-        client02.publish("v1/devices/me/telemetry", json.dumps(payload))
-
-        time.sleep(5)
-    
-        if evm_velocity > 3:
-            temp_set = temp_set - 1
-            if temp_set == 20:
-                AC_20()
-            elif temp_set == 21:
-                AC_21()
-            elif temp_set == 22:
-                AC_22()
-            elif temp_set == 23:
-                AC_23()
-            elif temp_set == 24:
-                AC_24()
-            elif temp_set == 25:
-                AC_25()
-            elif temp_set == 26:
-                AC_26()
-            elif temp_set == 27:
-                AC_27()
-            
-        if evm_velocity < 2:
-            temp_set = temp_set + 1
-            if temp_set == 20:
-                AC_20()
-            elif temp_set == 21:
-                AC_21()
-            elif temp_set == 22:
-                AC_22()
-            elif temp_set == 23:
-                AC_23()
-            elif temp_set == 24:
-                AC_24()
-            elif temp_set == 25:
-                AC_25()
-            elif temp_set == 26:
-                AC_26()
-            elif temp_set == 27:
-                AC_27()
-        time.sleep(5)
+        
         
